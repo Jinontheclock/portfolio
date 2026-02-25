b521806157982a9a10cc1430e65782a6dcd14e91
Jinontheclock <hlee563@my.bcit.ca>
2026-02-25 13:21:20 -0800

    manage window screen size

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+if (typeof document !== "undefined" && typeof navigator !== "undefined" && /Windows/i.test(navigator.userAgent)) {
+  document.documentElement.classList.add("platform-windows");
+}
+
 createRoot(document.getElementById("root")!).render(
   <>
     <App />
